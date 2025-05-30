38edb20d6c28290f7add1a5314ea75c53eb18340
NotAlexNoyle <alexnoyle@icloud.com>
2025-05-30 12:31:43 -0400

    Update gradle to 8.14.1

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("com.gradleup.shadow") version "8.3.5" // Import shadow API.
+    java // Tell gradle this is a java project.
+    eclipse // Import eclipse plugin for IDE integration.
+    kotlin("jvm") version "2.1.21" // Import kotlin jvm plugin for kotlin/java integration.
+}
+
+java {
+    // Declare java version.
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "me.barny1094875" // Declare bundle identifier.
+version = "1.19.2" // Declare plugin version (will be in .jar).
+val apiVersion = "1.19" // Declare minecraft server target version.
+
+tasks.named<ProcessResources>("processResources") {
+    val props = mapOf(
+        "version" to version,
+        "apiVersion" to apiVersion
+    )
+
+    inputs.properties(props) // Indicates to rerun if version changes.
+
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven {
+        url = uri("https://repo.purpurmc.org/snapshots")
+    }
+}
+
+dependencies {
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare purpur API version to be packaged.
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3") // Import MiniPlaceholders API.
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible builds.
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("") // Use empty string instead of null
+    from("LICENSE") {
+        into("/")
+    }
+    exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
+    minimize()
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.jar {
+    archiveClassifier.set("part")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+    options.compilerArgs.add("-Xlint:deprecation") // Triggers deprecation warning messages.
+    options.encoding = "UTF-8"
+    options.isFork = true
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+        vendor = JvmVendorSpec.GRAAL_VM
+    }
+}
